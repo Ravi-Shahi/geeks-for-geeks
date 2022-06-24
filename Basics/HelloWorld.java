22ca15cf6e013520a6793fefab84016c7c96c975
Ravi Shahi <ravishahi1810@gmail.com>
2022-06-24 23:30:08 +0530

    initial commit

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,16 @@
+/* 
+- Every Java code has to be part of a class
+- Always collection of class
+ */
+
+public class HelloWorld{
+    public static void main(String []args){ 
+     /*
+      * public : because main will be used outside the Class
+        static: it is special method, that can run without making object of function
+        void: return type
+        (String []args): parameter of type String of array of name args
+      */
+        System.out.println("Hello World");
+    }
+}
