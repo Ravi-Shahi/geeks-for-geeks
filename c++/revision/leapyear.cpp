d04d44c768410bdc9eded2011710ba8cb6381914
Ravi Shahi <ravishahi1810@gmail.com>
2022-07-19 06:18:22 +0530

    leap year

--- c++/revision/leapyear.cpp
+++ c++/revision/leapyear.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 
 int main(){
-    cout<<"Enter year: "<<endl;
     int year;
-    cin>> year;
+    cout<<"Enter year: "<<endl;
+    cin>>year;
+    // accuracy around 80%
     if(year%4)
-      cout<<"Not a Leap Year";
-    else if(year%100)
-      cout<<"Not a Leap Year";
+    cout<<"Not Leap Year"<<endl;
+    else if (year%100)
+    cout<<"Leap Year"<<endl;
     else if(year%400)
-      cout<<"Not a Leap Year";
+    cout<<"Not a Leap Year"<<endl;
     else
-      cout<<"Leap Year";
-    return 0;
+    cout<<"Leap Year"<<endl;
+    cout<<"Trying new approach"<<endl;
+    if(year%4==0 && year%100!=0||year%400==0)
+    cout<<"Leap Year"<<endl;
+    else
+    cout<<"Not a Leap Year"<<endl;
+    
 }
